9d30136967feadcc2056d7dd2e50056e3740902c
Ashraful Alam <ashrafulalamyt@gmail.com>
2024-09-08 09:27:18 +0600

    Update 1003.cpp

---
+++ beginner/1003.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int a,b,sum;
+    cin >> a >> b;
+    sum = a + b;
+    cout << "SOMA = " << sum << endl;
+    return 0;
+}
