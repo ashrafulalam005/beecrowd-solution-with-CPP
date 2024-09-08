0392e6e0a5327210195ff493427cafd3725af813
Ashraful Alam <ashrafulalamyt@gmail.com>
2024-09-08 09:12:31 +0600

    Update 1001.cpp

--- beginner/1001.cpp
+++ beginner/1001.cpp
@@ -3,9 +3,9 @@
 #include <stdio.h>
 int main()
 {
- int A,B,X;
- scanf("%d %d", &A, &B);
- X = A + B;
- printf("X = %d\n", X);
+    int A,B,X;
+    scanf("%d %d", &A, &B);
+    X = A + B;
+    printf("X = %d\n", X);
     return 0;
 }
