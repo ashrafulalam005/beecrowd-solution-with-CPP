e01ac2bf16a8b27c6d94b30160f399f297bedcab
Ashraful Alam <ashrafulalamyt@gmail.com>
2024-09-08 09:28:45 +0600

    Update 1004.cpp

---
+++ beginner/1004.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int a,b,product;
+    cin >> a >> b;
+    product = a*b;
+    cout << "PROD = " << product << endl;
+    return 0;
+}
